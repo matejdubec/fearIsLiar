9f525484d2667ea93be4aad93165a1e7f09dd9c3
ZasedackaVR Aldagames <universal.aldagames@gmail.com>
2020-08-05 16:53:05 +0200

    locomotion

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -12,13 +12,15 @@ public class PlayerController : MonoBehaviour
     private CharacterController characterController;
     private float gravityForce = 9.81f;
 
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if(input.axis.magnitude > 1)
-		{
-            Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
-            characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, gravityForce, 0) * Time.deltaTime);
-        }
+        Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
+        characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, gravityForce, 0) * Time.deltaTime);
     }
 }
