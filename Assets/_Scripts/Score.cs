c63717c30da226beb43029b05b2f7cb87d7230a1
Matej Dubec <xdubec@mendelu.cz>
2020-08-09 20:30:08 +0200

    collectables and score

---
+++ Assets/_Scripts/Score.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Score : MonoBehaviour
+{
+    private int score = 0;
+
+    [SerializeField]
+    private Text scoreText;
+
+    [SerializeField]
+    private Transform[] collectibles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Spheres collected: " + score.ToString() + "/" + collectibles.Length.ToString();
+    }
+
+    public void AddScore()
+	{
+        score++;
+     
+	}
+}
