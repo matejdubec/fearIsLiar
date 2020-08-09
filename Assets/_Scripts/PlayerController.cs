033d5ddcb78f1bad1e68a9ce464965f135ea215c
Matej Dubec <xdubec@mendelu.cz>
2020-08-09 21:50:13 +0200

    respawn boundary, falling acceleration

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -10,7 +10,8 @@ public class PlayerController : MonoBehaviour
     public SteamVR_Action_Vector2 input;
     public float speed = 1;
     private CharacterController characterController;
-    private float gravityForce = 9.81f;
+    private float fallingVelocity, gravityForce = 9.81f;
+    private float respawnBoundary = -45;
 
     private void Start()
     {
@@ -19,8 +20,20 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {      
         Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
-        characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, gravityForce, 0) * Time.deltaTime);
+        characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, fallingVelocity, 0) * Time.deltaTime);
+
+        if (!characterController.isGrounded)
+		{
+            fallingVelocity = fallingVelocity + (gravityForce * Time.deltaTime);
+        }
+		else
+		{
+            fallingVelocity = gravityForce;
+		}
+
+        if (Player.instance.hmdTransform.position.y < respawnBoundary)
+            Player.instance.hmdTransform.position = new Vector3(0, 0, 0);
     }
 }
