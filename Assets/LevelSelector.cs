77dfcc14673d043b42b3d638627625cbce05f4bd
Matej Dubec <xdubec@mendelu.cz>
2020-08-13 21:55:12 +0200

    level select

---
+++ Assets/LevelSelector.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+	public void SelectLevel( string levelName)
+	{
+		SceneManager.LoadScene(levelName);
+	}
+}
