5201dcc12a9a42f365ba340e2c3924d7c4707cc0
ZasedackaVR Aldagames <universal.aldagames@gmail.com>
2020-10-06 18:52:32 +0200

    pokus o novy inventory system

---
+++ Assets/_Scripts/CustomHandExperimental.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class CustomHandExperimental : Hand
+{
+    private FixedJoint m_Joint = null;
+
+    private SlotExperimental slot;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        m_Joint = GetComponent<FixedJoint>();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        if (grabGripAction.GetStateDown(trackedObject.inputSource))
+        {
+            if (slot)
+            {
+                slot.ReleaseObject(this);
+            }
+            else
+            {
+                AttachObject(slot.StoredObject.gameObject, GrabTypes.Grip);
+            }  
+        }
+
+        if (grabGripAction.GetStateUp(trackedObject.inputSource))
+        {
+            if (slot)
+            {
+                slot.AttachObject(this);
+            }
+            else
+            {
+                DetachObject(this.currentAttachedObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        slot = other.gameObject.GetComponent<SlotExperimental>();        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (slot)
+        {
+            slot = null;
+        }
+    }
+}
---
+++ Assets/_Scripts/SlotExperimental.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class SlotExperimental : MonoBehaviour
+{
+	private Valve.VR.InteractionSystem.Interactable storedObject = null;
+    public Valve.VR.InteractionSystem.Interactable StoredObject { get { return storedObject; } }
+    private FixedJoint joint = null;
+	private Vector3 minimazeFactor = new Vector3( 0.5f, 0.5f, 0.5f);
+
+	private void Awake()
+	{
+		joint = GetComponent<FixedJoint>();
+	}
+
+    public void ReleaseObject(Hand hand)
+	{
+		if (hand)
+		{
+			if (!hand.currentAttachedObject && storedObject)
+			{
+				storedObject.transform.localScale = new Vector3(
+					storedObject.transform.localScale.x / minimazeFactor.x,
+					storedObject.transform.localScale.y / minimazeFactor.y,
+					storedObject.transform.localScale.z / minimazeFactor.z);
+                storedObject.GetComponent<Collider>().enabled = true;
+                hand.AttachObject(storedObject.gameObject, GrabTypes.Grip);
+				joint.connectedBody = null;
+				storedObject = null;
+			}
+
+		}
+	}
+
+	public void AttachObject(Hand hand)
+	{
+		if (hand)
+		{
+			if (hand.currentAttachedObject)
+			{
+				storedObject = hand.currentAttachedObject.GetComponent<Valve.VR.InteractionSystem.Interactable>();
+				hand.DetachObject(hand.currentAttachedObject);
+
+				storedObject.transform.position = this.transform.position;
+				storedObject.transform.rotation = Quaternion.identity;
+				storedObject.transform.localScale = Vector3.Scale(storedObject.transform.localScale, minimazeFactor);
+                storedObject.GetComponent<Collider>().enabled = false;
+
+				Rigidbody targetBody = storedObject.gameObject.GetComponent<Rigidbody>();
+				joint.connectedBody = targetBody;              
+			}
+		}
+	}
+}
