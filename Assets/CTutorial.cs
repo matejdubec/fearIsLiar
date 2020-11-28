74e5ad9fc35641f43e68e3989e7d60e11760363a
Matej Dubec <xdubec@mendelu.cz>
2020-11-28 20:08:38 +0100

    tutorial

---
+++ Assets/CTutorial.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//https://answers.unity.com/questions/1490192/how-can-i-highlight-the-vive-controller-buttons.html
+public class CTutorial : MonoBehaviour
+{
+
+    [SerializeField] private GameObject canvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void WaypointReached()
+	{
+
+	}
+}
