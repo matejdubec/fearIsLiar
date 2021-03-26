477b95f67533d49e29358f63f406268fd1945765
Universal Alda Games <universal.aldagames@gmail.com>
2021-03-26 20:19:29 +0100

    basement

--- Assets/CDrawer.cs
+++ Assets/CDrawer.cs
@@ -6,21 +6,18 @@ public class CDrawer : MonoBehaviour
 {
 
     [SerializeField] private CInteractable top;
-    [SerializeField] private Transform controlledTransform;
-    private Vector3 leverBaseForward;
+    [SerializeField] private float maxOpenRange = 0.3f;
     private Vector3 topOriginPosition;
     private Vector3 prevHandPosition = Vector3.zero;
 
-    private float offset = 0.0f;
-    private Vector3 controlledTransformOriginPosition;
-    [SerializeField ]private Transform controlledTransformEndPosition;
+    private float controlledTransformOriginXPosition;
+    private float controlledTransformEndXPosition = 0.0f;
 
     private void Awake()
     {
-        this.leverBaseForward = (top.transform.position - controlledTransform.position).normalized;
         topOriginPosition = top.transform.localPosition;
-        offset = Vector3.Distance(top.transform.position, controlledTransform.position);
-        controlledTransformOriginPosition = controlledTransform.position;
+        controlledTransformOriginXPosition = transform.localPosition.x;
+        controlledTransformEndXPosition = controlledTransformOriginXPosition + maxOpenRange;
     }
 
     private void Update()
@@ -39,23 +36,14 @@ public class CDrawer : MonoBehaviour
         {
             if (prevHandPosition != Vector3.zero)
             {
-                //float diff = prevHandPosition.x - other.transform.position.x;
-                //float diff2 = controlledTransform.localPosition.x - diff;
-                //if (diff2 > controlledTransformOriginPosition.x && diff2 < controlledTransformEndPosition.localPosition.x)
-                //{
-                //    //controlledTransform.position = new Vector3(controlledTransform.position.x, controlledTransform.position.y, diff2);
-                //    controlledTransform.Translate(new Vector3(diff2, 0, 0) * (8 * Time.deltaTime));
-                //}
-
-                float diff = prevHandPosition.z - other.transform.position.z;
-                float diff2 = controlledTransform.position.z - diff;
-                if (diff2 > controlledTransformOriginPosition.z && diff2 < controlledTransformEndPosition.position.z)
+                float diff = transform.InverseTransformDirection(other.transform.localPosition - prevHandPosition).x;
+                float shift = transform.localPosition.x + diff;
+                if (controlledTransformOriginXPosition < shift && shift < controlledTransformEndXPosition)
                 {
-                    controlledTransform.position = new Vector3(controlledTransform.position.x, controlledTransform.position.y, diff2);
-                   
+                    transform.Translate(Vector3.right * diff);
                 }
             }
-            prevHandPosition =  other.transform.position;
+            prevHandPosition =  other.transform.localPosition;
         }
     }
 }
