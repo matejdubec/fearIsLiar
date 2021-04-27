147db78670101455571940330868ec80ee83b3e4
Universal Alda Games <universal.aldagames@gmail.com>
2021-04-27 19:30:43 +0200

    skyscrapers testers feedback

---
+++ Assets/_Scripts/COrbit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COrbit : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(dir);
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, rotation, Time.deltaTime);
+        transform.Translate(0, 0, 6 * Time.deltaTime);
+    }
+}
