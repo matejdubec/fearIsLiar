c887f252d5c7a67d6be111e1e5058b47570469f8
Universal Alda Games <universal.aldagames@gmail.com>
2021-05-02 18:57:31 +0200

    basement testers feedback

---
+++ Assets/CSpiderIdle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSpiderIdle : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Hand"))
+        {
+            animator.SetBool("doAttack", true);
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hand"))
+        {
+            animator.SetBool("doAttack", false);
+        }
+    }
+}
